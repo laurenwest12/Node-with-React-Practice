c685083257158e0e5afd08ad9eb9524ba1e28d49
Lauren West <laurenwest12@gmail.com>
2020-04-17 14:26:53 -0400

    refactoring promises

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,18 +23,30 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          //we already have an instance for this user id
-          done(null, existingUser);
-        } else {
-          // we don't have an instance with this user id so make a new instance
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        //we already have an instance for this user id
+        done(null, existingUser);
+      } else {
+        // we don't have an instance with this user id so make a new instance
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
+    // (accessToken, refreshToken, profile, done) => {
+    //   User.findOne({ googleId: profile.id })
+    //   .then((existingUser) => {
+    //     if (existingUser) {
+    //       //we already have an instance for this user id
+    //       done(null, existingUser);
+    //     } else {
+    //       // we don't have an instance with this user id so make a new instance
+    //       new User({ googleId: profile.id })
+    //         .save()
+    //         .then((user) => done(null, user));
+    //     }
+    //   });
+    // }
   )
 );
