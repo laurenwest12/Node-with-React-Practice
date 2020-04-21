6d00a8ba168d8cc174b70a0965b4098cf8ada7fc
Lauren West <laurenwest12@gmail.com>
2020-04-21 17:16:31 -0400

    updated header based on whether a user is logged in

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -4,10 +4,10 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
@@ -17,10 +17,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
         <div className="container">
           <BrowserRouter>
             <div>
+              <Header />
               <Route exact path="/" component={Landing} />
               <Route exact path="/surveys" component={Dashboard} />
               <Route path="/surveys/new" component={SurveyNew} />
