f72829baad48ada384a57a7a67ea16d3cb4384ac
Lauren West <laurenwest12@gmail.com>
2020-04-21 16:43:17 -0400

    updated header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
       <nav>
@@ -8,7 +27,7 @@ export default class Header extends Component {
           <a className="left brand-logo">Emaily</a>
           <ul className="right">
             <li>
-              <a>Login With Google</a>
+              <a>{this.renderContent()}</a>
             </li>
           </ul>
         </div>
@@ -16,3 +35,9 @@ export default class Header extends Component {
     );
   }
 }
+
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
