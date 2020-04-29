2e89383e243e2b319b5449ffbab925780123b510
Lauren West <laurenwest12@gmail.com>
2020-04-29 17:27:03 -0400

    fixing mongoose errors

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
+
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: mongoose.connect(process.env.MONGO_URI, {
+    useMongoClient: true,
+    connectWithNoPrimary: true,
+  }),
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
