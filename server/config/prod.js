0f9dddca214ff8f47f5e8a4cec8b11b710d291d1
Lauren West <laurenwest12@gmail.com>
2020-04-29 17:30:46 -0400

    fixing prod

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: mongoose.connect(process.env.MONGO_URI, {
-    useMongoClient: true,
-    connectWithNoPrimary: true,
-  }),
+  mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
