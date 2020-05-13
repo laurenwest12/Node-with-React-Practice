721899ba07c47c624ef934166adae97daeddb09e
Lauren West <laurenwest12@gmail.com>
2020-05-13 16:51:49 -0400

    updated form styling and functionality

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,35 +1,28 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' },
+];
 
 class SurveyForm extends Component {
   renderFields() {
     return (
       <div>
-        <Field
-          label="Survey Title"
-          type="text"
-          name="title"
-          component={SurveyField}
-        />
-        <Field
-          label="Subject Line"
-          type="text"
-          name="subject"
-          component={SurveyField}
-        />
-        <Field
-          label="Email Body"
-          type="text"
-          name="body"
-          component={SurveyField}
-        />
-        <Field
-          label="Recipient List"
-          type="text"
-          name="emails"
-          component={SurveyField}
-        />
+        {FIELDS.map(({ label, name }) => (
+          <Field
+            key={name}
+            label={label}
+            type="text"
+            name={name}
+            component={SurveyField}
+          />
+        ))}
       </div>
     );
   }
@@ -41,7 +34,13 @@ class SurveyForm extends Component {
           onSubmit={this.props.handleSubmit((values) => console.log(values))}
         >
           {this.renderFields()}
-          <button tyoe="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button tyoe="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
