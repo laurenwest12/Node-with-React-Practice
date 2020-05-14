abb9233be6e60234c20c09ea43a0e8bdcd54f414
Lauren West <laurenwest12@gmail.com>
2020-05-13 23:36:26 -0400

    added initial survey review component

--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = { showFormReview: false };
+
   render() {
     return (
       <div>
-        <SurveyForm />
+        {this.state.showFormReview ? (
+          <SurveyFormReview />
+        ) : (
+          <SurveyForm
+            onSurveySubmit={() => this.setState({ showFormReview: true })}
+          />
+        )}
       </div>
     );
   }
